9443f6703b01b8b5ebfb1995db687e018ed44b95
seanmoylan <sean.cheezy.moylan@gmail.com>
2019-11-26 14:52:29 +0000

    Added seperate client project

---
+++ gRPCClient/src/main/java/ie/gmit/sean/App.java
@@ -0,0 +1,25 @@
+package ie.gmit.sean;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App extends Application<Configuration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public void initialize(Bootstrap<Configuration> b) {
+    }
+
+    @Override
+    public void run(Configuration c, Environment e) throws Exception {
+        LOGGER.info("Registering REST resources");
+        e.jersey().register(new UsersRESTController(e.getValidator()));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new App().run(args);
+    }
+}
---
+++ gRPCClient/src/main/java/ie/gmit/sean/UserDB.java
@@ -0,0 +1,31 @@
+package ie.gmit.sean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserDB {
+    public static HashMap<Integer, User> Users = new HashMap<>();
+
+    static {
+        Users.put(1, new User(1, "Sean Moylan", "seanmoylan1@icloud.com", "password", " ", " "));
+        Users.put(2, new User(2, "Conor Moylan", "conormoylan1@icloud.com", "password2", " ", " "));
+    }
+
+    public static List<User> getUsers(){
+        return new ArrayList<User>(Users.values());
+    }
+
+    public static User getUser(Integer id){
+        return Users.get(id);
+    }
+
+    public static void updateUser(Integer id, User user){
+        Users.put(id, user);
+    }
+
+    public static void removeUser(Integer id){
+        Users.remove(id);
+    }
+}
+
